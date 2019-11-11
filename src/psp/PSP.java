886256872fba17622784dac523736acbc4825e6d
Daniel Florez <Daniel Florez@LAPTOP-J64PD6EP.home>
2019-11-11 22:53:57 +0100

    proyecto psp

---
+++ src/psp/PSP.java
@@ -0,0 +1,15 @@
+package psp;
+
+/*
+ * @author Daniel Florez
+ */
+public class PSP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+    }
+    
+}
